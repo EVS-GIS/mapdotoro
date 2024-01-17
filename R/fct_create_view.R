b8083835ae22e81dd8b17f9bfbd721c38d1da225
Manière Louis <louismaniere@orange.fr>
2024-01-17 17:05:14 +0100

    add continuity_width_full_side_geom view function

--- R/fct_create_view.R
+++ R/fct_create_view.R
@@ -196,3 +196,67 @@ continuity_full_side_view_geom <- function(db_con, view_name = "continuity_area_
 
   return(glue::glue("{view_name} view successfully created"))
 }
+
+#' Create continuity_width_full_side_geom view
+#'
+#' @param db_con database connection parameters.
+#' @param view_name view name.
+#'
+#' @importFrom glue glue
+#' @importFrom DBI dbExecute
+#'
+#' @return text
+#' @export
+continuity_width_full_side_view_geom <- function(db_con,
+                                                 view_name = "continuity_width_full_side_geom",
+                                                 continuity_table_name){
+  query <- glue::glue("
+    DO $$
+    DECLARE
+        continuity text;
+        query text;
+    BEGIN
+        query := 'CREATE OR REPLACE VIEW {view_name} AS
+		SELECT
+				hydro_swaths.gid, hydro_swaths.axis, hydro_swaths.measure_from_outlet,
+				hydro_swaths.strahler, hydro_swaths.gid_region, {continuity_table_name}_full_side.*,
+				ST_SetSRID(hydro_swaths.geom, 4326) AS geom
+			FROM hydro_swaths
+			LEFT JOIN (
+                  SELECT
+                      measure_medial_axis,
+                      hydro_swaths_gid,
+                      ';
+
+        -- Constructing the SELECT part of the query with SUM left and right side
+        FOR continuity IN (SELECT column_name FROM information_schema.columns
+    						WHERE table_name = '{continuity_table_name}'
+    							AND column_name NOT IN ('axis', 'measure_medial_axis',
+    												'side', 'id', 'hydro_swaths_gid'))
+    	-- Concatenate the query
+        LOOP
+            query := query || 'SUM(' || continuity || ') AS ' || continuity || ', ';
+        END LOOP;
+
+        -- Removing the trailing comma and space
+        query := LEFT(query, LENGTH(query) - 2);
+
+        -- Adding the FROM and GROUP BY parts of the query
+        query := query || '
+                  FROM
+                      {continuity_table_name}
+                  GROUP BY
+                      axis,
+                      measure_medial_axis,
+                      hydro_swaths_gid) AS {continuity_table_name}_full_side
+					  	ON {continuity_table_name}_full_side.hydro_swaths_gid = hydro_swaths.gid';
+
+        -- RAISE NOTICE 'Query: %', query;
+
+        EXECUTE query;
+    END $$;
+    ")
+  dbExecute(db_con, query)
+
+  return(glue::glue("{view_name} view successfully created"))
+}
